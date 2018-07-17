8f51067fbcedbc83ff92d2431d272e32333d33ea
pl3nny <pl3nny@gmail.com>
2018-07-16 17:02:56 -0700

    beginning to add setters and getters for the sol sys class...planet, jovian classes later and maybe star class as well

--- solar system/solarSystem.cpp
+++ solar system/solarSystem.cpp
@@ -1,2 +1,38 @@
 #include "solarSystem.h"
 
+solarSystem::solarSystem(){}
+
+void solarSystem::setMass(double mass)
+{
+	this.mass = mass;
+}
+
+double solarSystem::getMass()
+{
+	return mass;
+}
+
+void solarSystem::setVolume(double volume)
+{
+	this.volume = volume;
+}
+
+double solarSystem::getVolume()
+{
+	return volume;
+}
+
+void solarSystem::setRadius(double radius)
+{
+	this.radius = radius;
+}
+
+double solarSystem::getRadius()
+{
+	return radius;
+}
+
+void calcVolume()
+{
+	return 
+}
--- solar system/solarSystem.h
+++ solar system/solarSystem.h
@@ -1,5 +1,22 @@
 class solarSystem
 {
 private:
-	int celestBodies; // celestial bodies
+	double mass;
+	double volume;
+	double diameter;
+	double radius;
+	
+public:
+	void setMass(double);
+	double getMass();
+	
+	void setVolume(double);
+	double getVolume();
+
+	void setRadius(double);
+	double getRadius();
+
+	void calcVolume();
+	void calcDiameter();
+	void calcMass();
 };
