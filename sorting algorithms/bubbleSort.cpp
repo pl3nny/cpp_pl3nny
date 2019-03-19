a38660b18d34c71e81de9cb791e30c46cbc96f51
pl3nny <pl3nny@gmail.com>
2019-03-19 07:02:43 -0700

    bubbleSort almost done

--- sorting algorithms/bubbleSort.cpp
+++ sorting algorithms/bubbleSort.cpp
@@ -1,14 +1,55 @@
 /*
 Author: Alexander Hernandez
 title: Bubble Sort
-Date: Summer 2018
+Date: Spring 2019
 */
 #include <iostream>
+#include <array>
 
 using namespace std;
 
-int main()
-{
+int swap(int arr[], int index, int size);
 
+int main() 
+{
+	cout << "Author: Alexander Hernandez" << endl;
+	cout << "\tBubbleSort\n" << endl;
+
+	int arr[] =  {10, 1, 4, 11, 20, -30, 8};
+	int temp;
+
+	for (int i = 0; i < 7; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	// BubbleSorting Algorithm
+
+	for (int i = 7 - 1; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 7; ++i)
+	{
+		cout << arr[i] << " "; 
+	}
+
+	cout << endl;
+
+	return 0;
+}
+
+int swap(int arr[], int index, int size) 
+{
 	return 0;
 }
