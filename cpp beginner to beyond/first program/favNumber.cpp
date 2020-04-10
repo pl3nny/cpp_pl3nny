bfc49dbd6a7c374a9f4806c7cb6fa3248990ba08
pl3nny <pl3nny@gmail.com>
2020-04-09 21:54:35 -0700

    started c++ review

---
+++ cpp beginner to beyond/first program/favNumber.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main ()
+{
+    int fav_number;
+
+    std::cout << "Enter your favorite number: ";
+    std::cin >> fav_number;
+    std::cout << "That's my fav number too!" << std::endl;
+}
