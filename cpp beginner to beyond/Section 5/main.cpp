9ed30cc8d146fc52226a0b2e764f059d491f0097
pl3nny <pl3nny@gmail.com>
2020-04-10 00:29:43 -0700

    update

---
+++ cpp beginner to beyond/Section 5/main.cpp
@@ -0,0 +1,15 @@
+/****************************
+ * Author: pl3nny
+****************************/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    cout << "Author: pl3nny" << endl;
+    return 0;
+}
