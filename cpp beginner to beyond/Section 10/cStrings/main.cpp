8b2e37d40b7a917c652897dfce718b17f8de98a1
pl3nny <pl3nny@gmail.com>
2020-04-15 09:39:57 -0700

    update

--- cpp beginner to beyond/Section 10/cStrings/main.cpp
+++ cpp beginner to beyond/Section 10/cStrings/main.cpp
@@ -17,10 +17,11 @@ int main()
     
     size_t whole_name_length{strlen(whole_name)};
 
+    cout << endl;
     cout << "The length of the first name, " << first_name << ", is " 
     << first_name_length << " letters long and the length of the last name, " 
     << last_name << ", is " << last_name_length 
     << " letters long. This means that the length of the whole name must be " 
-    << whole_name_length << " letters long.";
+    << whole_name_length << " letters long." << endl;
     return 0;
 }
