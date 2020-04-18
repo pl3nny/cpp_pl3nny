bebae2dfb7a93dd65d5ff8719e63bfefad5ca490
pl3nny <pl3nny@gmail.com>
2020-04-18 14:40:00 -0700

    added comments to pyramid assignment code

--- cpp to beyond/Section 10/pyramid_assignment/main.cpp
+++ cpp to beyond/Section 10/pyramid_assignment/main.cpp
@@ -11,16 +11,19 @@ int main()
 
     for (size_t i = 0; i < user_input.size(); i++)
     {
+        // print out white spaces
         for (size_t j = user_input.size() - 1; j > i; j--)
         {
             cout << " ";
         }
 
+        // print out characters in order
         for (size_t k = 0; k <= i; k++)
         {
             cout << user_input.at(k);
         }
 
+        // print out characters in reverse order - 1
         for (size_t l = i; l > 0; l--)
         {
             cout << user_input.at(l-1);
