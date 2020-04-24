e0408c60dbe286f0b64db940d7d87b12c53271bd
pl3nny <pl3nny@gmail.com>
2020-04-24 01:58:56 -0700

    section 11 recursive functions

---
+++ cpp to beyond/Section 11/fibonacciRecursion/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int num);
+int main()
+{
+    cout << fib(10) << endl;
+    return 0;
+}
+
+int fib(int num)
+{
+    if (num == 0)
+        return 0;
+    if (num == 1)
+        return 1;
+
+    return fib(num - 1) + fib(num - 2);
+}
