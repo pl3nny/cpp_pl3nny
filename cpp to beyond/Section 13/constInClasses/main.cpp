a40b9a9d1e64ffc68e19c12e3dea9fe7eda0472e
pl3nny <pl3nny@gmail.com>
2020-05-20 22:16:27 -0700

    working on const in classes

---
+++ cpp to beyond/Section 13/constInClasses/main.cpp
@@ -0,0 +1,21 @@
+// Section 13
+// const in classes
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player
+{
+    private:
+        string name;
+        int health;
+        int xp;
+    public:
+};
+
+int main()
+{
+
+    return 0;
+}
