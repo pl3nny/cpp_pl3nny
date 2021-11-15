2eb5eaba7071f654ba7e0a7f1b3ac076e8f6d692
pl3nny <alexander.hernandez.ra6@gmail.com>
2021-11-14 22:13:28 -0800

    update

--- cpp to beyond/Section 6/primitiveTypes/main.cpp
+++ cpp to beyond/Section 6/primitiveTypes/main.cpp
@@ -9,16 +9,16 @@ int main()
      *  Character Type
     **************************/
 
-    char middle_initial{'R'}; // single quote for char double quotes for strings
+    char middle_initial {'R'}; // single quote for char double quotes for strings
     cout << "My middle initial is " << middle_initial << endl;
 
     /**************************
      *  Integer Types
     **************************/
-    unsigned short int exam_score{55};
+    unsigned short int exam_score {55};
     cout << "My exam score was " << exam_score << endl;
 
-    int countries_represented{65};
+    int countries_represented {65};
     cout << "There were " << countries_represented << " countries represented in my meeting" << endl;
 
     long people_in_florida {20610000};
