b25c6a1a6fc3629b3df34a1fbeea1387563f28a4
pl3nny <alexander.hernandez.ra6@gmail.com>
2025-03-03 23:01:56 -0800

    wrapping up section 14

---
+++ cpp to beyond/Section 14/Challenge/Mystring.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <cstring>
+#include "Mystring.h"
+
+ // No-args constructor
+Mystring::Mystring() 
+    : str{nullptr} {
+    str = new char[1];
+    *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) 
+    : str {nullptr} {
+        if (s==nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[strlen(s)+1];
+            strcpy(str, s);
+        }
+}
+
+// Copy constructor
+Mystring::Mystring(const Mystring &source) 
+    : str{nullptr} {
+        str = new char[strlen(source.str)+ 1];
+        strcpy(str, source.str);
+ //       std::cout << "Copy constructor used" << std::endl;
+
+}
+
+// Move constructor
+Mystring::Mystring( Mystring &&source) 
+    :str(source.str) {
+        source.str = nullptr;
+//        std::cout << "Move constructor used" << std::endl;
+}
+
+ // Destructor
+Mystring::~Mystring() {
+    delete [] str;
+}
+
+ // Copy assignment
+Mystring &Mystring::operator=(const Mystring &rhs) {
+//    std::cout << "Using copy assignment" << std::endl;
+
+    if (this == &rhs) 
+        return *this;
+    delete [] str;
+    str = new char[strlen(rhs.str) + 1];
+    strcpy(str, rhs.str);
+    return *this;
+}
+
+// Move assignment
+Mystring &Mystring::operator=( Mystring &&rhs) {
+ //   std::cout << "Using move assignment" << std::endl;
+    if (this == &rhs) 
+        return *this;
+    delete [] str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+
+
+// Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+ // getters
+ int Mystring::get_length() const { return strlen(str); }
+ const char *Mystring::get_str() const { return str; }
+
+// overloaded insertion operator
+std::ostream &operator<<(std::ostream &os, const Mystring &rhs) {
+    os << rhs.str;
+    return os;
+}
+
+// overloaded extraction operator
+std::istream &operator>>(std::istream &in, Mystring &rhs) {
+    char *buff = new char[1000];
+    in >> buff;
+    rhs = Mystring{buff};
+    delete [] buff;
+    return in;
+}
+
+// - unary minus overloaded operator
+Mystring Mystring::operator-() const {
+    char *buff = new char [std::strlen(this->str) + 1];
+    std::strcpy(buff, this->str);
+    for(size_t i = 0; i < std::strlen(buff); i++) {
+        buff[i] = std::tolower(buff[i]);
+    }
+    Mystring temp {buff};
+    delete [] buff;
+    return temp;
+}
+
+// addition overloaded operator - concatenate 
+Mystring Mystring::operator+(const Mystring &rhs) const {
+    char *buff = new char [std::strlen(this->str) + std::strlen(rhs.str) + 1];
+    std::strcpy(buff,this->str);
+    std::strcat(buff,rhs.str);
+
+    Mystring temp {buff};
+    delete [] buff;
+
+    return temp;
+}
+
+// == overloaded comparison operator
+bool Mystring::operator==(const Mystring &rhs) const {
+    return std::strcmp(this->str, rhs.str) == 0;
+}
+
+// overloaded not equal operator
+bool Mystring::operator!=(const Mystring &rhs) const {
+    return std::strcmp(this->str, rhs.str) != 0;
+}
+
+// overloaded less than operator
+bool Mystring::operator<(const Mystring &rhs) const {
+    return this->str < rhs.str;
+}
+
+// overloaded greater than operator
+bool Mystring::operator>(const Mystring &rhs) const {
+    return this->str > rhs.str;
+}
+
+// DOES NOT WORK CORRECTLY -- GO BACK AND FIX
+// overloaded += operator
+Mystring Mystring::operator+=(const Mystring &rhs) {
+    return operator+(rhs.str);
+}
---
+++ cpp to beyond/Section 14/Challenge/Mystring.h
@@ -0,0 +1,37 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+    friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
+    friend std::istream &operator>>(std::istream &in, Mystring &rhs);
+
+private:
+    char *str;      // pointer to a char[] that holds a C-style string
+public:
+    Mystring();                                                         // No-args constructor
+    Mystring(const char *s);                                     // Overloaded constructor
+    Mystring(const Mystring &source);                    // Copy constructor
+    Mystring( Mystring &&source);                         // Move constructor
+    ~Mystring();                                                     // Destructor
+    
+    Mystring &operator=(const Mystring &rhs);     // Copy assignment
+    Mystring &operator=(Mystring &&rhs);           // Move assignment
+    
+    void display() const;
+    
+    int get_length() const;                                      // getters
+    const char *get_str() const;
+
+    Mystring operator-() const;         // uniary minus overloaded operator
+    Mystring operator+(const Mystring &rhs) const;      // addition overloaded operator - concatenate
+
+    bool operator==(const Mystring &rhs) const;     // overlaoded comparison operator
+    bool operator!=(const Mystring &rhs) const;     // overloaded not equal operator
+    bool operator<(const Mystring &rhs) const;      // overloaded less than operator
+    bool operator>(const Mystring &rhs) const;      // overloaded greater than operator
+
+    Mystring operator+=(const Mystring &rhs);       // overlaoded += operator
+};
+
+#endif // _MYSTRING_H_
