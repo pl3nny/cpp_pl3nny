b193d537a0d913676e4d42087d6069bf866444f8
Alex H <pl3nny@Alexs-MacBook-Air.local>
2025-03-16 21:31:18 -0700

    adding adjusted files for section 15

--- cpp to beyond/Section 15/ProtectedMembersClassAccess/main.cpp
+++ cpp to beyond/Section 15/ProtectedMembersClassAccess/main.cpp
@@ -16,15 +16,31 @@ class Base {
 };
 
 class Derived: public Base {
+  // Note friends of Derived have access to only what Derived has access to
+
+  // a will be public
+  // b will be protected
+  // c will not be accessible
+
+  public:
+    void access_base_members() {
+      a = 100; // ok
+      b = 200; // ok
+      // c = 300;
+    }
+};
+
   int main() {
     cout << "=== Base member access from base objects =============" << endl;
     Base base;
-    base.a == 100;
+    base.a = 100;
     // base.b == 200; // compiler error because data is protected; objects that are protected or private will produce compiler errors
     // base.c == 300; // compiler error because data is private
 
     cout << "=== Base member access from derived objects =============" << endl;
-
+    Derived d;
+    d.a = 100;
+    // d.b = 200;
+    //d.c = 300;
     return 0;
-  }  
-};
+  }
